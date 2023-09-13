9f323f70c1f1fe7275cc6c577a756068da7179a5
Tony Eseh <esehtony123@gmail.com>
2023-09-13 02:24:29 +0100

    added the attributes to the user model

--- agwuse/users/models.py
+++ agwuse/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CASCADE, CharField, DateField, EmailField, ForeignKey, IntegerField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -11,10 +11,39 @@ class User(AbstractUser):
     check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
 
+    GENDER_TYPES = [("M", "Male"), ("F", "Female")]
+    MARITAL_CHOICES = [("M", "Married"), ("S", "Single")]
+    OCCUPATION_CHOICES = [
+        ("Civil Servant", "Civil Servant"),
+        ("Business Owner", "Business Owner"),
+        ("Trader", "Trader"),
+        ("Student", "Student"),
+        ("Unemployed", "Unemployed"),
+    ]
+    DEPARTMENT_CHOICES = [
+        ("MEN", "MEN"),
+        ("WOMEN", "WOMEN"),
+        ("YOUTH", "YOUTH"),
+        ("CHILDREN", "CHILDREN"),
+        ("YOUNG SINGLE", "YOUNG SINGLE"),
+        ("TEENAGERS", "TEENAGERS"),
+    ]
     # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    middle_name = CharField(_("Middle name of User"), blank=True, max_length=50)
+    first_name = CharField(_("First Name of User"), max_length=50)  # type: ignore
+    last_name = CharField(_("Last name of User"), max_length=50)  # type: ignore
+    dob = DateField(_("Date of Birth"))
+    sex = CharField(_("Gender"), choices=GENDER_TYPES)
+    username = CharField(_("Username"), max_length=50)
+    marital_status = CharField(_("Marital Status"), choices=MARITAL_CHOICES)
+    password = CharField(_("User Password"))
+    email = EmailField(_("User Email"), unique=True)
+    phone_number = CharField(_("User Phone Number"), max_length=20)
+    occupation = CharField(_("User Occupation"), choices=OCCUPATION_CHOICES)
+    card_no = IntegerField()
+    department = ForeignKey("Departments", on_delete=CASCADE)
+    unit = ForeignKey("Units", on_delete=CASCADE)
+    # post =
 
     def get_absolute_url(self) -> str:
         """Get URL for user's detail view.
